d950e8678d4f70d0fb7de792d7b55dc268683f77
nitishshar <nitishshar@gmail.com>
2021-03-02 03:06:45 +0530

    Made changes to fetch schema and data

--- saccr/src/parquet/parquet.controller.ts
+++ saccr/src/parquet/parquet.controller.ts
@@ -4,12 +4,9 @@ import { ParquetService } from './parquet.service';
 @Controller('parquet')
 export class ParquetController {
     constructor(private parquetService: ParquetService) { }
-//     @Get()
-//    async getData() {
-//        return await this.parquetService.getUserData();
-//     }
     @Get()
-   async getMetData() {
-       return await this.parquetService.getMetadata();
+    async getData() {
+        return await this.parquetService.getUserData();
     }
+
 }
--- saccr/src/parquet/parquet.service.ts
+++ saccr/src/parquet/parquet.service.ts
@@ -3,25 +3,27 @@ var parquet = require('parquetjs-lite');
 @Injectable()
 export class ParquetService {
     async getUserData(): Promise<any> {
-        const response: any[] = [];
+        const responseData: any[] = [];
+        const schema: any[] = [];
         let reader = await parquet.ParquetReader.openFile('C://userdata.parquet');
+        const columns: any[] = reader.metadata.schema.map((el: any) => el.name);
         let cursor = reader.getCursor();
-
         // read all records from the file and print them
         let record = null;
         while (record = await cursor.next()) {
-            response.push(record);
+            responseData.push(record);
         }
+        columns.forEach((col: string) => {
+            schema.push({ name: col, type: this.getPropertyType(responseData[0], col)||'string' });
+        });
         await reader.close();
-        return response;
+        return { columns: schema, data: responseData };
     }
-    async getMetadata(): Promise<any> {
-        //const response: any[] = [];
-        let reader = await parquet.ParquetReader.openFile('C://userdata.parquet');
-        
-        let row_groups=reader.metadata;
-        await reader.close();
-        
-        return row_groups;
+
+    public getPropertyValue<T, K extends keyof T>(obj: T, key: K) {
+        return obj[key];
+    }
+    public getPropertyType<T, K extends keyof T>(obj: T, key: K) {
+        return typeof obj[key];
     }
 }
